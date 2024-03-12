2d3d1457b15994ff6bf49ffd553a217bf87a0039
Marcus Willock <crazcalm@gmail.com>
2024-03-12 19:29:39 -0400

    Almost done deducing the poker hand type

--- my_books/practical_sorting_book/code/poker/src/main.rs
+++ my_books/practical_sorting_book/code/poker/src/main.rs
@@ -1,7 +1,8 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Suite {
     Heart,
     Club,
@@ -98,6 +99,109 @@ impl PokerHand {
         self.cards.push(card);
         Ok(())
     }
+
+    fn is_flush(&self) -> bool {
+        let mut result = true;
+
+        let suite = &self.cards[0].suite;
+        for card in &self.cards[1..] {
+            if card.suite != *suite {
+                result = false;
+                break;
+            }
+        }
+
+        result
+    }
+
+    fn card_rank_histogram(&self) -> Vec<(u8, usize)> {
+        let mut map = HashMap::new();
+
+        for card in &self.cards {
+            map.entry(card.name)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        }
+
+        let mut results: Vec<(u8, usize)> = map
+            .iter()
+            .map(|(k, v)| (k.clone(), v.clone() as usize))
+            .collect();
+        results.sort_by(|a, b| a.1.cmp(&b.1).then(a.0.cmp(&b.0)).reverse());
+
+        results
+    }
+
+    fn have_straight(&self) -> bool {
+        // TODO: Add unit tests
+        let mut ranks: Vec<u8> = self.cards.iter().map(|card| card.name.clone()).collect();
+
+        ranks.sort();
+
+        match ranks[0] {
+            2 => ranks == [2, 3, 4, 5, 14] || ranks == [2, 3, 4, 5, 6],
+            _ => {
+                let mut result = true;
+
+                let mut expected = ranks[0];
+                for num in ranks {
+                    if num != expected {
+                        result = false;
+                        break;
+                    }
+
+                    expected += 1
+                }
+
+                result
+            }
+        }
+    }
+
+    fn set_hand_type(&mut self) -> Result<(), &'static str> {
+        // TODO: Add unit tests
+        if self.cards.len() != 5 {
+            return Err("Must have 5 Cards to set hand type");
+        }
+
+        let card_rank_histogram = self.card_rank_histogram();
+
+        // Check Poker hand Types that have multiple cards of the same rank
+        if card_rank_histogram[0].1 == 4 {
+            self.poker_hand_type = Some(PokerHandType::FourOfAKind);
+        } else if card_rank_histogram[0].1 == 3 && card_rank_histogram[1].1 == 2 {
+            self.poker_hand_type = Some(PokerHandType::FullHouse);
+        } else if card_rank_histogram[0].1 == 3 && card_rank_histogram[1].1 == 1 {
+            self.poker_hand_type = Some(PokerHandType::ThreeOfAKind);
+        } else if card_rank_histogram[0].1 == 2 && card_rank_histogram[1].1 == 2 {
+            self.poker_hand_type = Some(PokerHandType::TwoPair);
+        } else if card_rank_histogram[0].1 == 2 && card_rank_histogram[1].1 == 1 {
+            self.poker_hand_type = Some(PokerHandType::Pair)
+        }
+
+        // Check to see if we should return early
+        if self.poker_hand_type.is_some() {
+            return Ok(());
+        }
+
+        //TODO change is_flush to has_flush
+        match (self.have_straight(), self.is_flush()) {
+            (false, false) => self.poker_hand_type = Some(PokerHandType::HighCard),
+            (false, true) => self.poker_hand_type = Some(PokerHandType::Flush),
+            (true, false) => self.poker_hand_type = Some(PokerHandType::Straight),
+            (true, true) => {
+                let mut ranks: Vec<u8> = self.cards.iter().map(|card| card.name.clone()).collect();
+                ranks.sort();
+                if ranks[0] == 10 {
+                    self.poker_hand_type = Some(PokerHandType::RoyalFlush);
+                } else {
+                    self.poker_hand_type = Some(PokerHandType::StraightFlush);
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
 fn main() {
@@ -107,5 +211,61 @@ fn main() {
         hand.add_card(card).expect("Too many Cards!!!");
 
         println!("{:?}", hand);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Too many Cards!!!")]
+    fn test_too_many_cards() {
+        let mut hand = PokerHand::new();
+        for x in 2..8 {
+            let card = Card::new(x, Suite::Heart).unwrap();
+            hand.add_card(card).expect("Too many Cards!!!");
+
+            println!("{:?}", hand);
+        }
+    }
+
+    #[test]
+    fn test_card_rank_histogram() {
+        let mut hand = PokerHand::new();
+
+        for x in [6, 8, 4, 4, 8] {
+            let card = Card::new(x, Suite::Heart).unwrap();
+            hand.add_card(card).unwrap();
+        }
+
+        let result = hand.card_rank_histogram();
+
+        assert_eq!(result, vec![(8, 2), (4, 2), (6, 1)])
+    }
+
+    #[test]
+    fn test_is_flush_true() {
+        let mut hand = PokerHand::new();
+
+        for x in [6, 8, 4, 4, 8] {
+            let card = Card::new(x, Suite::Heart).unwrap();
+            hand.add_card(card).unwrap();
+        }
+
+        assert!(hand.is_flush());
+    }
+
+    #[test]
+    fn test_is_flush_false() {
+        let mut hand = PokerHand::new();
+
+        for x in [6, 8, 4, 4] {
+            let card = Card::new(x, Suite::Heart).unwrap();
+            hand.add_card(card).unwrap();
+        }
+        hand.add_card(Card::new(2, Suite::Spade).unwrap()).unwrap();
+
+        assert_eq!(hand.is_flush(), false);
     }
 }
